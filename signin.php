cfdf3a4ed8c60ac35809bc1e3f8c8a10114c13a2
ricepower <lenika@naver.com>
2024-08-15 17:26:50 +0900

    first

---
+++ signin.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/signin-style.css">
+    <title>TEE LENS Order</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="form-container sign-in">
+            <form action="starter-template-sample.html">
+                <h1>Sign In</h1>
+                <span>or use your email password</span>
+                <br />
+                <input type="text" placeholder="ID or Email">
+                <input type="password" placeholder="Password">
+                <a href="#">Forget Your Password? 아이디 기억하기 넣기</a>
+                <button>Sign In</button>
+            </form>
+        </div>
+        <div class="logo-container">
+            <div class="logo">
+                <div class="logo-panel logo-right">
+                    <div class="logo-img">
+                        <img src="assets/img/logo.png" />
+                    </div>
+                    <h1>TEE LENS</h1>
+                    <p>teelens.order 방문을 환영한다잉</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
